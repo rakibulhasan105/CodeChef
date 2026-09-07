86ec2d13b2b208b5538574f27ca996ebb0e34b38
Rakibul Hasan <rakibulhasan105105@gmail.com>
2026-09-08 00:43:13 +0600

    here we go...

---
+++ Chef_and_Instant_Noodles.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+int main() {
+	// your code goes here
+	int stoves,minutes,max_number;
+	scanf("%d %d",&stoves, &minutes);
+	max_number=stoves*minutes;
+	printf("%d",max_number);
+	return 0;
+
+}
