a1a805ad9092f2694de6b586798c4fd5f0beac55
Rakibul Hasan <rakibulhasan105105@gmail.com>
2026-09-08 00:45:41 +0600

    Implement basic input/output in Fitness.cpp

---
+++ Fitness.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    
+    while (t--) {
+        int x;
+        scanf("%d", &x);
+        // Your code goes here
+        printf("%d\n",x*2*5);
+        
+    }
+    
+}
